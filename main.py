d9fc622910bd1225c21a1b2a5e6b81a93a65a153
Hachem Sfar <35065444+hachemsfar@users.noreply.github.com>
2023-03-19 19:57:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import streamlit as st
+import openai
 
-st.write("Hello, World!")
+openai.api_key = "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"
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature = 0.7,
+        max_tokens = 1024,
+        top_p = 1,
+        frequency_penalty = 0.5,
+        presence_penalty = 0.5
+    )
+
+    return response.choices[0].text.strip()
+
+
+user_input = "How to cook Fricassé?  in html"
+response = generate_response(user_input)
+print(response)
+
+
+st.write(response)
