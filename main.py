6e3dbe54109510f420fbf8ae2074347d73c971ce
Hachem Sfar <35065444+hachemsfar@users.noreply.github.com>
2023-03-20 01:33:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def get_recipe(dish_name):
     )
 
     response = completion.choices[0].text
-    st.write(response)
+    #st.write(response)
 
     API_URL = "https://api-inference.huggingface.co/models/edwardjross/xlm-roberta-base-finetuned-recipe-all"
     API_TOKEN= st.secrets["API_TOKEN"]
